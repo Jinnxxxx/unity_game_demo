c74f7e621db1aa405a9b0d9e47da2298719dac72
Jinnxxxx <707626422@qq.com>
2026-05-06 18:18:03 +0800

    update

---
+++ Assets/EntityFx.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityFx : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+
+    [Header("Flash FX")]
+    [SerializeField] private Material hitMat;
+    [SerializeField] private Material orignalMat;
+
+    
+
+}
